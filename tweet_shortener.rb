1ce7471dc030a81207b9e7b62f35e77dbc4c88c3
Jack Yeung <jyeung.se@gmail.com>
2018-06-20 20:29:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+
+def dictionary
+  sustitute = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(array_tweets)
+  array_tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+
+
